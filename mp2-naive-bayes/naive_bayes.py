c7e82721e899bb9c0e9d727a997597b246a115d0
shivenk78 <kumar.shiven15@gmail.com>
2021-02-22 17:30:06 -0500

    MP2 Initial Commit

---
+++ mp2-naive-bayes/naive_bayes.py
@@ -0,0 +1,41 @@
+# naive_bayes.py
+# ---------------
+# Licensing Information:  You are free to use or extend this projects for
+# educational purposes provided that (1) you do not distribute or publish
+# solutions, (2) you retain this notice, and (3) you provide clear
+# attribution to the University of Illinois at Urbana-Champaign
+#
+# Created by Justin Lizama (jlizama2@illinois.edu) on 09/28/2018
+
+"""
+This is the main entry point for Part 1 of this MP. You should only modify code
+within this file for Part 1 -- the unrevised staff files will be used for all other
+files and classes when code is run, so be careful to not modify anything else.
+"""
+
+
+
+def naiveBayes(train_set, train_labels, dev_set, smoothing_parameter, pos_prior):
+    """
+    train_set - List of list of words corresponding with each email
+    example: suppose I had two emails 'like this movie' and 'i fall asleep' in my training set
+    Then train_set := [['like','this','movie'], ['i','fall','asleep']]
+
+    train_labels - List of labels corresponding with train_set
+    example: Suppose I had two emails, first one was ham and second one was spam.
+    Then train_labels := [1, 0]
+
+    dev_set - List of list of words corresponding with each email that we are testing on
+              It follows the same format as train_set
+
+    smoothing_parameter - The smoothing parameter --laplace (1.0 by default)
+    pos_prior - positive prior probability (between 0 and 1)
+    """
+    # TODO: Write your code here
+    # return predicted labels of development set
+
+
+
+
+    return []
+    
